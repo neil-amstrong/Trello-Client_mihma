fbecdecc7b17c13428d706a05ceec6b102c8f9d0
neil-amstrong <neilamrstrong92@gmail.com>
2025-08-18 15:35:33 +0100

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,11 @@
+sudo apt update && sudo apt upgrade -y
+echo "Install required dependencies"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y 
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl status docker
+sudo systemctl enable docker
+sudo systemctl start docker
+#sudo usermod -aG docker $docke
